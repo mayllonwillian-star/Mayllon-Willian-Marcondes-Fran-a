81accc87974ebce849c94eb831a0271c52ec4f15
Mayllon Willian Marcondes França <mayllon.willian@unifesp.br>
2026-05-14 14:48:08 -0300

    Add C program to calculate distance between points

--- 1000.c
+++ 1000.c
@@ -1,1 +1,24 @@
-100
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Mayllon Willian Marcondes França
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1071
+Data        : 14/05/2026
+Objetivo    : Calcular a distância entre dois pontos
+Aprendizado : Utilizar a biblioteca de matemática com a raiz quadrada
+-------------------------------------------------------------------------- */
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float x1, y1, x2, y2;
+    float distancia;
+    scanf("%f", &x1);
+    scanf("%f", &y1);
+    scanf("%f", &x2);
+    scanf("%f", &y2);
+    distancia = sqrt( (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) );
+    printf("%.4f\n", distancia);
+    return 0;
+}
